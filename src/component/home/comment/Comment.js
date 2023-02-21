77e4d900dfb104820a8eb059cd506daba2602563
jangjaehyuk <wogur11178@gmail.com>
2023-02-21 11:18:10 +0900

    first commit

---
+++ src/component/home/comment/Comment.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Comment = () => {
+    return(
+        <div className="comment-wrapper">
+            <div className='comment-form'>
+                <div className='inp-box'>
+                    <input type='text' />
+                </div>
+                <div>
+                    <button>완료</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {applyMiddleware, compose, createStore} from "redux";
+// import rootReducer from "./reducers";
+import {logger} from "redux-logger/src";
+
+
+
+const enhancer =
+    process.env.NODE_ENV === "production"
+        ? compose(applyMiddleware())
+        : composeWithDevTools(applyMiddleware(logger));
+
+// const store = createStore(rootReducer, enhancer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
