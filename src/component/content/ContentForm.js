4dddefc998c278123226d924052843dc2d1f4048
NonamedBread <lgh778923@gmail.com>
2023-02-21 17:35:32 +0900

    add : addContents-div

--- src/component/content/ContentForm.js
+++ src/component/content/ContentForm.js
@@ -1,24 +1,52 @@
 import { React, useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import { Link } from "react-router-dom";
+import { ko } from "date-fns/esm/locale";
 import "react-datepicker/dist/react-datepicker.css";
 import "./contentForm.scss";
-
 const ContentForm = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [count, setCount] = useState(0);
+  const [divs, setDivs] = useState([]);
+  const [Contents, setContents] = useState([]);
+
+  const addSubject = (e) => {
+    console.log(e);
+  };
+
+  const addDiv = () => {
+    setCount(count + 1);
+    setDivs((prevDivs) => [
+      ...prevDivs,
+      <input
+        key={count}
+        type="text"
+        placeholder="해야 할 일을 작성해주세요!!"
+      />,
+    ]);
+  };
+
   useEffect(() => {
-    console.log(startDate);
-  }, [startDate]);
+    console.log(count);
+  }, [count]);
+
   return (
     <div className="content-form__container">
       <div className="content-form__wrapper">
         <div className="content-form">
-          <div className="content-form__subject">제목</div>
-          <input type="text" placeholder="제목을 입력해주세요." />
-          <div>
+          <div className="subject__wrapper">
+            <div className="content-form__subject">제목</div>
+            <input
+              type="text"
+              onChange={addSubject}
+              placeholder="제목을 입력해주세요."
+            />
+          </div>
+          <div className="datePicker__wrapper">
             <div className="content-form__startDate">시작일</div>
-            <div>
+            <div className="start-date-picker">
               <DatePicker
+                locale={ko}
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
                 showTimeSelect
@@ -29,8 +57,9 @@ const ContentForm = () => {
               />
             </div>
             <div className="content-form__endDate">종료일</div>
-            <div>
+            <div className="end-date-picker">
               <DatePicker
+                locale={ko}
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
                 showTimeSelect
@@ -41,10 +70,19 @@ const ContentForm = () => {
               />
             </div>
           </div>
-
-          <div className="content">내용</div>
-          <div className="content-form__save">저장</div>
-          <div className="content-form__cancel">
+          <div className="content">
+            <input
+              key={count}
+              type="text"
+              placeholder={"해야 할 일을 작성해주세요!!"}
+            />
+            {divs}
+          </div>
+          <div className="content-form__btn" onClick={addDiv}>
+            추가
+          </div>
+          <div className="content-form__btn">저장</div>
+          <div className="content-form__btn">
             <Link to="/">취소</Link>
           </div>
         </div>
