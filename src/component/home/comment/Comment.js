39a006d87de362e1689244811b2087ec4ed2ef50
jangjaehyuk <wogur11178@gmail.com>
2023-02-21 11:27:03 +0900

    comment page

--- src/component/home/comment/Comment.js
+++ src/component/home/comment/Comment.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import './comment.scss';
 const Comment = () => {
     return(
         <div className="comment-wrapper">
             <div className='comment-form'>
                 <div className='inp-box'>
-                    <input type='text' />
+                    <input type='text' placeholder='댓글을 작성해주세요.' />
                 </div>
-                <div>
+                <div className='comment-submit'>
                     <button>완료</button>
                 </div>
             </div>
